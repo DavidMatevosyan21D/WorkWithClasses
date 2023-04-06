74724d2d6d5f467ca5915f73750231b7d3183d8b
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2023-04-06 18:58:34 +0300

    Add files via upload

---
+++ Zadanie2_Classes/Zadanie2_Classes/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2_Classes
+{
+    public class Music
+    {
+        public string ArtistName { get; set; }
+        public string SongTitle { get; set; }
+        public long ReleaseDate { get; set; }
+        public Music(string artistName, string songTitle, long releaseDate)
+        {
+            ArtistName = artistName;
+            SongTitle = songTitle;
+            ReleaseDate = releaseDate;
+        }
+        public override string ToString()
+        {
+            return "Имя Артиста - " + ArtistName + "  Назавние песни - "  + SongTitle + "  Год выхода песни: " + ReleaseDate; 
+        }
+    }
+
+}
---
+++ Zadanie2_Classes/Zadanie2_Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using Zadanie2_Classes;
+
+Music[] musics = new Music[5];
+
+
+for (int i = 0; i < musics.Length; i++)
+{
+    Console.WriteLine("Имя артиста:");
+    string artistName = Console.ReadLine();
+    Console.WriteLine("Название песни:");
+    string songTitle = Console.ReadLine();
+    Console.WriteLine("Год выхода:");
+    long releaseDate = Convert.ToInt64(Console.ReadLine());
+    Console.WriteLine("\n");
+    musics[i] = new Music(artistName, songTitle, releaseDate);
+}
+for (int i = 0; i < musics.Length; i++)
+{
+    Console.WriteLine(musics[i]);
+}
